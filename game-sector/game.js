49a5f07b33a51ad16dc2600c8d374ad6f61ce81b
jimacsx <maximino@kamikazelab.com>
2017-04-06 19:55:50 -0500

    terminando juego de sector cine

--- game-sector/game.js
+++ game-sector/game.js
@@ -1,61 +1,57 @@
 window.onload = function() {
-
-	// game definition, 100% of browser window dimension
-	var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio,Phaser.CANVAS,"",{
+	//la definición del juego como, el 100% de la dimensión de la ventana del navegador
+	var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio,Phaser.CANVAS,"gameDiv",{
 		preload:onPreload,
 		create:onCreate,
 		update:onUpdate,
-		resize:onResize // <- this will be called each time the game is resized
+		resize:onResize // <- esta se deberá llamar cada ves que el juego es reized
 	});
-
+	//variables utilizadas dentro del juegp
 	var gameStarted = false;
-
 	var pausa = false;
-	// first function to be called, when the game preloads I am loading the sprite sheet with all game tiles
+	var cherryArray = [];
+	var oscarArray = [];
+	var posxLeo = 0;
+
+	var nivel = 0;
+	var subir = false;
+	//primer function que es llamada, cuando el juego precarga
+	//todos los elementos del juego se cargan
 	function onPreload() {
-		game.load.spritesheet('logo','logo-sectorcine.png');
-		game.load.spritesheet('carrete','carrete.png');
-		game.load.spritesheet('leo','leo.png');
-		//game.load.spritesheet('triste','triste.png');
-		game.load.spritesheet('linea','line-score.png');
-		game.load.spritesheet('upsText','ups.png');
-		game.load.spritesheet('button','boton.png', 193, 71);
-		game.load.spritesheet('button2','boton2.png', 193, 71);
-		game.load.spritesheet('instructionsText','instructions.png');
-		game.load.image('move','line-score.png', 32, 48);
-		game.load.image('move2','line-score.png', 32, 48);
-		game.load.image('oscar','oscar.png');
-		game.load.image('cherry','cherry.png');
-		game.load.audio('punto','numkey.wav');
+		game.load.spritesheet('logo','assets/images/logo-sectorcine.png');
+		game.load.spritesheet('carrete','assets/images/carrete.png');
+		game.load.spritesheet('leo','assets/images/leo.png', 77, 169);
+		game.load.spritesheet('linea','assets/images/line-score.png');
+		game.load.spritesheet('upsText','assets/images/ups.png');
+		game.load.spritesheet('button','assets/images/boton.png');
+		game.load.spritesheet('button2','assets/images/boton2.png');
+		game.load.spritesheet('instructionsText','assets/images/instructions.png');
+		game.load.image('move','assets/images/line-score.png', 32, 48);
+		game.load.image('move2','assets/images/line-score.png', 32, 48);
+		game.load.image('oscar','assets/images/oscar.png');
+		game.load.image('cherry','assets/images/cherry.png');
+		game.load.audio('punto','assets/numkey.wav');
 	}
-
-	// function to scale up the game to full screen
-	function goFullScreen(){
-		// setting a background color
-		game.stage.backgroundColor = "#1d1f20";
-		game.scale.pageAlignHorizontally = true;
-		game.scale.pageAlignVertically = true;
-		// using RESIZE scale mode
-		game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-		game.scale.setScreenSize(true);
-	}
-
-	// function to be called when the game has been created
+	//function que se llama cuando el juego ha sido creado
 	function onCreate() {
-		// going full screen with the function defined at line 32
+		//hacemos full screen  del juego
 		goFullScreen();
-
+		//creamos todos los elementos del juego
 		logo = game.add.sprite(80,25, 'logo');
 		logo.width = 194;
 		logo.height = 68;
+		logo.scale.setTo(1,1);
 
 		carrete = game.add.sprite(0, game.world.height -75, 'carrete');
 		carrete.width = window.innerWidth;
 		carrete.height = 75;
+		carrete.scale.setTo(1,1);
 
-		leo = game.add.sprite(0, game.world.height - 100, 'leo');
-		leo.width = 77;
-		leo.height = 167;
+		leo = game.add.sprite(32, game.world.height - 100, 'leo');
+		leo.animations.add('neutro', [1], 10, true);
+    leo.animations.add('feliz', [0], 10, true);
+		leo.animations.add('triste', [2], 100, true);
+    leo.animations.play('neutro');
 
 		linea = game.add.sprite(0, 200, 'linea');
 		linea.width = 175;
@@ -75,29 +71,28 @@ window.onload = function() {
 			fill: "#ffffff"
 		});
 		scoreText2.anchor.y=1;
-
 		/*--------------------------- Pantalla 1 ------------------------- */
 		upsText = game.add.sprite(100,100,'upsText');
 		upsText.anchor.setTo(0.5, 0.5)
-		//upsText.width = 200;
-		//upsText.height = 100;
-		//  Standard button (also used as our pointer tracker)
+
 	 	boton = game.add.button(100, 100, 'button', '', this, 2, 1, 0);
+		boton.width = 223;
+		boton.height = 51;
 	 	boton.name = 'play';
 	 	boton.anchor.setTo(0.5, 0.5);
-    boton.onInputUp.add(actionPlay, this);
-		//boton.inputEnable = true;
+		boton.inputEnable = true;
 		boton.input.useHandCursor = true;
-
+    boton.onInputUp.add(actionPlay, this);
 		/*--------------------------- Pantalla 2 ------------------------- */
 		instructionsText = game.add.sprite(100,100,'instructionsText');
 		instructionsText.anchor.setTo(0.5, 0.5);
 		instructionsText.visible = false;
-
-		//Agregamos el move
- 		move = game.add.sprite(32, 10, 'move');
+		//Agregamos los moves
+		x = game.rnd.integerInRange(10, game.width);
+ 		move = game.add.sprite(x, 10, 'move');
 		move.visible = false;
-		move2 = game.add.sprite(32, 10, 'move2');
+		x2 = game.rnd.integerInRange(10, game.width);
+		move2 = game.add.sprite(x2, 10, 'move2');
 		move2.visible = false;
 		/*--------------------------- Pantalla 5 ------------------------- */
 		ups2Text = game.add.text(200, 250, '¡Ups!', {font:'34px Arial', fill: '#fff'});
@@ -108,54 +103,40 @@ window.onload = function() {
 		atrapasteText.visible = false;
 
 		boton2 = game.add.button(100, 100, 'button2', '', this, 2, 1, 0);
+		boton2.width = 223;
+		boton2.height = 51;
 	 	boton2.name = 'gameOver';
 	 	boton2.anchor.setTo(0.5, 0.5);
-    boton2.onInputUp.add(actionPlayAgain, this);
-		//boton2.inputEnable = true;
+		boton2.inputEnabled = true;
 		boton2.input.useHandCursor = true;
+    boton2.onInputUp.add(actionPlayAgain, this);
 		boton2.visible = false;
 		/*--------------------------- Agregamos la "fisica" ------------------------- */
 		game.physics.enable(leo, Phaser.Physics.ARCADE);
 		game.physics.enable(move, Phaser.Physics.ARCADE);
 		game.physics.enable(move2, Phaser.Physics.ARCADE);
+		game.physics.enable(carrete, Phaser.Physics.ARCADE);
 
     leo.body.collideWorldBounds = true;
     leo.body.bounce.set(1);
     leo.body.immovable = true;
-
-		//Para que el move no se caiga
- 		//move.body.gravity.y = 300;
+		//Para que los moves no se caigan
 		move.body.immovable = true;
-		//Empieza a correr
- 		move.body.velocity.x = 200;
-
 		move2.body.immovable = true;
-		//Empieza a correr
- 		move2.body.velocity.x = 80;
-
- 		//Variables del juego
- 		game.giro = 250;
- 		game.gravedadOscares = 150;
-
-		game.giro2 = 250;
- 		game.gravedadCherries = 150;
-
-		game.puntaje = 0;
+		//para cuando los oscares toquen el carrete estos desaparecen
+		carrete.body.collideWorldBounds = true;
+    carrete.body.bounce.set(1);
+    carrete.body.immovable = true;
+		//creamos los grupos de oscares y cherries
  		oscares = game.add.group();
 		cherries = game.add.group();
-		//Sonidos
+		//Sonido
     sndPunto = game.add.audio('punto');
-		//nivel
-		game.nivel = 1;
-		game.subirNivel = game.time.events.loop(10000, subirNivel, this);
-		// normall, onResize is called each time the browser is resized, anyway I am calling it the first time
-		// to place all responsive elements in their right positions.
+		//normalmente, la function onResize es llamada  cada ves que el navegador es resized,
+		//de cualquier forma llamo a esta function la primera ves
     onResize();
-
-		//game.input.onDown.add(restart, this);
-		// Add a input listener that can help us return from being paused
-    game.input.onDown.add(unpause, self);
 	}
+	//function que es activada cuando damos click en boton "JUGAR"
 	function actionPlay () {
 		//ocultamos texto y boton de pantalla 1
 		upsText.visible = false;
@@ -166,14 +147,213 @@ window.onload = function() {
 		move2.visible = true;
 		//iniciar juego
 		gameStarted = true;
+		//inicialiazar Variables
+		reinit();
 	}
+
+	function onUpdate() {
+		onResize();
+		//Si el juego ha empezado, se habilita el movimiento de LEO
+		if(gameStarted) {
+			//Ponemos delay para esconder instrucciones
+			game.time.events.add(3000, function() { instructionsText.destroy();  }, this);
+			//Mover a Leo con el cursor
+			leo.x = game.input.x;
+	    if(leo.x < 24) {
+	      leo.x = 24;
+	    } else if(leo.x > game.width - 75) {
+	      leo.x = game.width - 101;
+	    }
+			leo.y = (game.height-167)-75;
+
+			//tiramos oscares de acuerdo a la posicion de move
+			if(move.body.velocity.x > 0 && move.x > game.giro){
+	    	move.body.velocity.x *= -1;
+    		game.giro = game.rnd.integerInRange(100, move.x - 50);
+				soltarOscar();
+			}
+			if(move.body.velocity.x < 0 && move.x < game.giro){
+	    	move.body.velocity.x *= -1;
+    		game.giro = game.rnd.integerInRange(move.x + 50, game.width);
+				soltarOscar();
+			}
+
+			//tiramos cherries de acuerdo a la posicion de move2
+			if(move2.body.velocity.x > 0 && move2.x > game.giro2){
+	    	move2.body.velocity.x *= -1;
+    		game.giro2 = game.rnd.integerInRange(100, move2.x - 50);
+				console.log("soltarCherry");
+				soltarCherry();
+			}
+			if(move2.body.velocity.x < 0 && move2.x < game.giro2){
+	    	move2.body.velocity.x *= -1;
+    		game.giro2 = game.rnd.integerInRange(move2.x + 50, game.width);
+				soltarCherry();
+			}
+			//mandamos a llamar función recogerOscar cada que haya collision entre oscar y leo
+			game.physics.arcade.overlap(leo, oscares, recogerOscar, null, this);
+			//mandamos a llamar función gameOver cada que haya collision entre cherry y leo
+			game.physics.arcade.overlap(leo, cherries, gameOver, null, this);
+			//mandamos a llamar función killOscares cada que haya collision entre carrete y oscares
+			game.physics.arcade.overlap(carrete, oscares, killOscares, null, this);
+			//mandamos a llamar función killCherries cada que haya collision entre carrete y cherries
+			game.physics.arcade.overlap(carrete, cherries, killCherries, null, this);
+		} else {
+			if(pausa){
+				leo.x = posxLeo;
+			}
+		}
+	}
+
+	function soltarOscar() {
+		xrandom = move.x;
+		if(xrandom > game.width - 80) {
+			xrandom = xrandom - 80;
+		} else if(xrandom < 24){
+			xrandom = 80;
+		} else {
+			xrandom = xrandom;
+		}
+		oscar = oscares.create(xrandom, -50, 'oscar');
+    game.physics.arcade.enable(oscar);
+		console.log("gravedadOscares: " +Math.round(game.gravedadOscares));
+    oscar.body.gravity.y = Math.round(game.gravedadOscares);
+		//guardamos oscares para poder hacer la Pausa del juego
+		oscarArray.push(oscar);
+	}
+
+	function soltarCherry() {
+		xrandom = move2.x;
+		if(xrandom > game.width - 80) {
+			xrandom = xrandom - 80;
+		} else if(xrandom < 24){
+			xrandom = 80;
+		} else {
+			xrandom = xrandom;
+		}
+		cherry = cherries.create(xrandom, -50, 'cherry');
+    game.physics.arcade.enable(cherry);
+    cherry.body.gravity.y = Math.round(game.gravedadCherries);
+		//guardamos cherries para poder hacer la Pausa del juego
+		cherryArray.push(cherry);
+	}
+	function recogerOscar(leo, oscar){
+ 		oscar.kill();
+ 		game.puntaje += 1;
+		score.setText(game.puntaje);
+		sndPunto.play();
+		game.time.events.add(300, function() { leo.animations.play('neutro');  }, this);
+		leo.animations.play('feliz');
+		//subimos nivel cada 10 de puntaje
+		subirNivel();
+	}
+	function killOscares(carrete, oscar){
+		oscar.kill();
+	}
+	function killCherries(carrete, cherry){
+		cherry.kill();
+	}
+	function gameOver(leo, cherry){
+		//detenemos moves que sueltan oscares y cherries
+		move.kill();
+		move2.kill();
+		//detenemos cherries para simular PAUSA del juego
+		cherryArray.filter(function(cherry) {
+			return cherry.body.gravity = false;
+		});
+		//detenemos oscares para simular PAUSA del juego
+		oscarArray.filter(function(oscar) {
+			return oscar.body.gravity = false;
+		});
+		//guardamos la posición en la que LEO tocó una cherry para poder detenerlo
+		posxLeo = game.input.x;
+		//cambiamos estado de leo
+		leo.animations.play('triste');
+		//mostramos texto y botón de "JUGAR OTRA VES"
+		ups2Text.visible = true;
+		atrapasteText.setText('Atrapaste '+game.puntaje+' estatuillas');
+		atrapasteText.visible = true;
+		boton2.visible = true;
+		//pausamos juego
+		gameStarted = false;
+		pausa = true;
+	}
+
+	function actionPlayAgain() {
+		//borramos texto y botón de "JUGAR OTRA VES"
+		ups2Text.visible = false;
+		atrapasteText.visible = false;
+		boton2.visible = false;
+		if(pausa) {
+			//matamos arreglo de cherries que se mantienen en pausa
+			cherryArray.filter(function(cherry) {
+				return cherry.kill();
+			});
+			cherryArray = [];
+			//matamos arreglo de oscares que se mantienen en pausa
+			oscarArray.filter(function(oscar) {
+				return oscar.kill();
+			});
+			//quitamos pausa en el juego
+			pausa = false;
+			//revivimos los moves que arrejan oscares y cherries
+			move.revive();
+	    move2.revive();
+			//reiniciamos Juego
+			reinit();
+			gameStarted = true;
+		}
+	}
+
+	function reinit(){
+		game.puntaje = 0;
+		score.setText(game.puntaje);
+		leo.animations.play('neutro');
+		//moves
+		move.body.velocity.x = 200;
+		move2.body.velocity.x = 80;
+		//giros
+		game.giro = 50;
+		game.gravedadOscares = 100;
+
+		game.giro2 = 250;
+		game.gravedadCherries = 100;
+	}
+
+	function subirNivel(){
+		if(game.puntaje % 10 == 0){
+			game.gravedadOscares *= 1.5;
+	 		move.body.velocity.x *= 0.5;
+
+			game.gravedadCherries *= 1.5;
+	 		move2.body.velocity.x *= 1.5;
+			nivel += 1;
+			console.log("nivel: " + nivel);
+		}
+	}
+
+	//function que hace full screen del juego
+	function goFullScreen(){
+		//asignamos el color de fondo del juego
+		game.stage.backgroundColor = "#1d1f20";
+		game.scale.pageAlignHorizontally = true;
+		game.scale.pageAlignVertically = true;
+		// using RESIZE scale mode
+		game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+		game.scale.setScreenSize(true);
+	}
+
+	//function que es llamada cada ves que el navegador hace resize, y re-posiciona
+	//los elementos del juego para mantener su posición deacuerdo al tamaño del juego
 	function onResize(){
-		// this function is called each time the browser is resized, and re-positions
-		// game elements to keep them in their right position according to game size
 		carrete.y = game.height-75;
 		carrete.width = window.innerWidth;
 
-		leo.x = Math.round((game.width-77)/2) ;
+		if(pausa){
+			leo.x = game.input.x;
+		}else{
+			leo.x = Math.round((game.width-77)/2) ;
+		}
 		leo.y = (game.height-167)-75;
 
 		upsText.x = game.world.centerX;
@@ -184,8 +364,10 @@ window.onload = function() {
 
 		score.x = game.width-score.width - 135;
 		score.y = 170;
+
 		linea.x = game.width-linea.width - 50;
 		linea.y = 220;
+
 		scoreText.x = game.width-scoreText.width - 50;
 		scoreText.y = 250;
 		scoreText2.x = game.width-scoreText2.width - 50;
@@ -202,164 +384,4 @@ window.onload = function() {
 		boton2.y = game.world.centerY + 40 ;
 	}
 
-	function onUpdate() {
-		//Si el juego ha empezado, se habilita el movimiento de LEO
-		if(gameStarted) {
-			console.log(game.nivel);
-			//Ponemos delay para esconder instrucciones
-			game.time.events.add(5000, function() { instructionsText.destroy();  }, this);
-			//Mover a Leo con el cursor
-			leo.x = game.input.x;
-	    if(leo.x < 24) {
-	      leo.x = 24;
-	    } else if(leo.x > game.width - 75) {
-	      leo.x = game.width - 101;
-	    }
-			leo.y = (game.height-167)-75;
-
-			//tiramos oscares de acuerdo a la posicion de move
-			if(move.body.velocity.x > 0 && move.x > game.giro){
-	    	move.body.velocity.x *= -1;
-    		game.giro = game.rnd.integerInRange(100, move.x - 1);
-				soltarOscar();
-			}
-			if(move.body.velocity.x < 0 && move.x < game.giro){
-	    	move.body.velocity.x *= -1;
-    		game.giro = game.rnd.integerInRange(move.x+1, game.width);
-				soltarOscar();
-			}
-
-			//tiramos cherries de acuerdo a la posicion de move2
-			if(move2.body.velocity.x > 0 && move2.x > game.giro2){
-	    	move2.body.velocity.x *= -1;
-    		game.giro2 = game.rnd.integerInRange(100, move2.x - 1);
-				soltarCherry();
-			}
-			if(move2.body.velocity.x < 0 && move2.x < game.giro2){
-	    	move2.body.velocity.x *= -1;
-    		game.giro2 = game.rnd.integerInRange(move2.x+1, game.width);
-				soltarCherry();
-			}
-			//mandamos a llamar función recogerOscar cada que haya collision entre oscar y leo
-			game.physics.arcade.overlap(leo, oscares, recogerOscar, null, this);
-			//mandamos a llamar función gameOver cada que haya collision entre cherry y leo
-			game.physics.arcade.overlap(leo, cherries, gameOver, null, this);
-
-			//onClick(pausa);
-		}
-	}
-
-	function soltarOscar() {
-		xrandom = move.x;
-		if(xrandom > game.width - 80) {
-			xrandom = xrandom - 80;
-		} else if(xrandom < 24){
-			xrandom = 80;
-		} else {
-			xrandom = xrandom;
-		}
-		oscar = oscares.create(xrandom, -50, 'oscar');
-    game.physics.arcade.enable(oscar);
-    oscar.body.gravity.y = game.gravedadOscares;
-	}
-
-	function soltarCherry() {
-		xrandom = move2.x;
-		if(xrandom > game.width - 80) {
-			xrandom = xrandom - 80;
-		} else if(xrandom < 24){
-			xrandom = 80;
-		} else {
-			xrandom = xrandom;
-		}
-		cherry = cherries.create(xrandom, -50, 'cherry');
-    game.physics.arcade.enable(cherry);
-    cherry.body.gravity.y = game.gravedadCherries;
-	}
-
-	function recogerOscar(leo, oscar){
- 		oscar.kill();
- 		game.puntaje += 1;
-		score.setText(game.puntaje);
-		sndPunto.play();
-	}
-
-	// And finally the method that handels the pause menu
-  function unpause(event){
-    // Only act if paused
-    if(game.paused){
-			// Unpause the game
-			console.log("despausar");
-      game.paused = false;
-			instructionsText.visible = true;
-			ups2Text.visible = false;
-			atrapasteText.visible = false;
-		}
-	}
-
-	function gameOver(leo, cherry){
-		//termina juego
-		//game.nivel = 0;
-		//gameStarted = false;
-		//move.kill();
-    //move2.kill();
-		//cherry.kill();
-		//pausamos juego
-		//console.log("gameOver");
-		//pausa = true;
-		game.paused = true;
-		/*ups2Text.visible = true;
-		atrapasteText.setText('Atrapaste '+game.puntaje+' estatuillas');
-		atrapasteText.visible = true;
-		boton2.visible = true;*/
-	}
-
-	function onClick(pausa) {
-		if(pausa){
-			game.paused = true;
-			ups2Text.visible = true;
-			atrapasteText.setText('Atrapaste '+game.puntaje+' estatuillas');
-			atrapasteText.visible = true;
-			boton2.visible = true;
-		}else{
-			game.resume = true;
-		}
-		pausa = !pausa;
-	}
-	function actionPlayAgain () {
-		console.log("play again");
-		//reiniciamos nivel
-		game.nivel = 1;
-		game.subirNivel = 0;
-		//iniciar juego de nuevo
-		//game.paused = false;
-		gameStarted = true;
-		//ocultamos texto y boton de pantalla 1
-		ups2Text.visible = false;
-		atrapasteText.visible = false;
-		boton2.visible = false;
-		game.puntaje = 0;
-		score.setText(game.puntaje);
-		move.visible = true;
-		move2.visible = true;
-		move.revive();
-    move2.revive();
-
-		return true;
-	}
-
-	function subirNivel(){
- 		game.gravedadOscares *= 1.2;
- 		move.body.velocity.x *= 1.2;
-
-		game.gravedadCherries *= 1.2;
- 		move2.body.velocity.x *= 1.2;
- 		game.nivel += 1;
- 		console.log('Nivel: '+game.nivel);
-	}
 }
-
-/*
-ball.body.velocity.setTo(0, 0);
-http://joelwalls.com/2015/07/tutorial-programacion-de-juegos-con-phaser-parte-2/
-*/
